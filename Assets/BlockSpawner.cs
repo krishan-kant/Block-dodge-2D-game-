137f48f5558c4b2f26568690cf9d8dbb8dded226
Krishan Kant <krish.kant.47@gmail.com>
2019-11-11 15:03:56 +0530

    Assets uploaded

---
+++ Assets/BlockSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    public Transform[] spawnpoints;
+    public GameObject blockPrefab;
+    // Start is called before the first frame update
+    public float timeBetweenWaves = 1f;
+    private float timeToSpawn = 2f;
+
+    void Update()
+    {
+        if(Time.time >= timeToSpawn)
+        {
+            SpawnBlocks();
+            timeToSpawn = Time.time + timeBetweenWaves;
+        }
+    }
+    
+    void SpawnBlocks()
+    {
+        int randomIndex = Random.Range(0, spawnpoints.Length);
+
+        for (int i = 0; i < spawnpoints.Length; i++)
+        {
+            if (randomIndex != i)
+            {
+                Instantiate(blockPrefab, spawnpoints[i].position, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 15f;
+    private Rigidbody2D rb;
+    public float mapWidth = 5f;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed ;
+        Vector2 newPosition = rb.position + Vector2.right * x;
+        newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+        rb.MovePosition(newPosition);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        FindObjectOfType<GameManager>().Endgame();
+    }
+}
